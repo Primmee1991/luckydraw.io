7fc7db1a07dc4581c76cb198f1a434884c1fcc0d
Primmee1991 <warot.pat@deksomboon.com>
2024-05-22 19:47:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -90,5 +90,5 @@ document.getElementById('drawButton').addEventListener('click', function() {
             spread: 100,
             origin: { y: 0.6 }
         });
-    }, 1000);  // 10 วินาที
+    }, 10000);  // 10 วินาที
 });
